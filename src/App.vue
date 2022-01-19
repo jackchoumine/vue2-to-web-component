468c40ab6371c44b92bbd56a133fba187b1bec94
jack <jackzhoumine@gmail.com>
2022-01-20 00:52:27 +0800

    修改组件你

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> | -->
+      <!-- <router-link to="/about">About</router-link> -->
+      <Home/>
     </div>
-    <router-view/>
+    <!-- <router-view/> -->
   </div>
 </template>
-
+<script>
+import Home from './views/Home.vue'
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Description : 
+ * @Date        : 2022-01-20 00:50:27 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2022-01-20 00:50:28 +0800
+ * @LastEditors : JackChou
+ */
+module.exports ={
+  publicPath: './',
+}
