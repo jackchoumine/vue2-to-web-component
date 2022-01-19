021ba9d14760d900f16174bb28888375f0011bcc
jack <jackzhoumine@gmail.com>
2022-01-20 01:09:30 +0800

    安装依赖

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 /*
- * @Description : 
+ * @Description :
  * @Date        : 2022-01-20 00:50:27 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-01-20 00:50:28 +0800
+ * @LastEditTime: 2022-01-20 01:08:59 +0800
  * @LastEditors : JackChou
  */
-module.exports ={
+module.exports = {
   publicPath: './',
+  outputDir: 'build'
 }
